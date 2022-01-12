518e17d6349b61ca4c9c35c892a511faa476c864
Ung Nhat Hang <hang.ung@executionlab.asia>
2022-01-12 23:24:49 +0700

    update roadmap

---
+++ src/components/GoldenText/index.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+import { Text } from "../Typo/Text";
+
+export const GoldenText = styled(Text)`
+  font-family: "Judson";
+  font-style: normal;
+  font-weight: bold;
+  /* background: linear-gradient(to bottom, #fff9d7, #e5c87f, #ba8623, #8f8466); */
+  background: linear-gradient(
+    27.48deg,
+    #8f6b2d 4.87%,
+    #f6c65c 32.49%,
+    #c2933a 47.27%,
+    #fdcc5f 62.04%
+  );
+  text-shadow: 0px 1px 2px rgba(186, 134, 35, 0.12);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+`;
---
+++ src/components/RoadMapLine/index.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const RoadMapLine = styled.div`
+  height: 100%;
+  background: linear-gradient(
+    180deg,
+    #fff9d7 22.07%,
+    #e6c87f 44.81%,
+    #ba8623 61.12%,
+    #8f8466 77.86%
+  );
+`;
