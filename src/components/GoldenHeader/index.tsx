328259cbb7db36c002c25964e6e147f0bea3918a
unhang11@gmail.com <unhang11@gmail.com>
2022-05-08 00:48:07 +0700

    title effect

--- src/components/GoldenHeader/index.tsx
+++ src/components/GoldenHeader/index.tsx
@@ -23,17 +23,20 @@ const H1 = styled.h1<BaseStyleProps>`
   font-weight: 700;
   line-height: 64px;
   font-family: "Red Rose", cursive;
+  /* text-shadow: 0px 0px 24px rgba(255, 221, 64, 0.25),
+  0px -2px 2px rgba(0, 0, 0, 0.5), 0px 2px 2px rgba(255, 255, 255, 0.4); */
+  /* text-shadow: 0px -2px 2px rgba(0, 0, 0, 0.5); */
+  color: transparent;
   background: linear-gradient(
     180deg,
     #fffad0 15.32%,
     #ffe1a6 34.9%,
     #bc824d 89.01%
   );
+  background-clip: text;
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
-  background-clip: text;
-  text-fill-color: transparent;
-  text-shadow: 0px 0px 24.8863px rgba(255, 221, 64, 0.25);
+  /* text-fill-color: transparent; */
   @media (max-width: 768px) {
     font-weight: 400;
     font-size: 30px;
